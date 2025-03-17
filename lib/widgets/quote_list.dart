371dcd26d0ddcbaa1c03c250cb53dfc071a00364
fkoraiche <roshanfahd@gmail.com>
2025-03-17 22:21:32 +0000

    flutter

---
+++ lib/widgets/quote_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
